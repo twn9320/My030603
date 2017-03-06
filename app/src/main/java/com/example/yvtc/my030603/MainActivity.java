fa952669dce68e5174dfdc9cd464897a47ccdfc1
twn9320 <twn9320@gmail.com>
2017-03-06 14:20:15 +0800

    OnItemSelected

--- app/src/main/java/com/example/yvtc/my030603/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my030603/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.yvtc.my030603;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textView);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView spitv=(TextView) view;
+                tv.setText(spitv.getText());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
